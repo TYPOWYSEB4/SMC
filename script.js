417c088b1a92c40c7d125aa37a91f1758a92858c
logyQT <stoltmann96@gmail.com>
2022-10-24 18:06:03 +0200

    yes sir ... no sir

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let e = document.querySelectorAll(".option");
+const container = document.querySelector(".main");
+let e = document.querySelector("nav").querySelectorAll(".option");
+let currPage = undefined;
 
 e.forEach((e) =>
     e.addEventListener("click", (c) => {
-        console.log(c.target.querySelector("a").href);
-        window.location.href = c.target.querySelector("a").href;
+        let page = c.target.getAttribute("page");
+        changePage(page);
     })
 );
+
+const changePage = (page) => {
+    if (currPage == page) return;
+    currPage = page;
+    container.innerHTML = "";
+    let temp = container.querySelectorAll("section").length;
+    if (temp) {
+        temp.forEach((e) => e.remove());
+    }
+    let iframe = document.createElement("iframe");
+    iframe.setAttribute("src", `pages/${page}.html`);
+    iframe.setAttribute("onload", "this.insertAdjacentHTML('afterend', (this.contentDocument.body||this.contentDocument).innerHTML);this.remove()");
+    iframe.setAttribute("style", "display: none");
+    container.append(iframe);
+};
+
+changePage("home");
