5b779c282de2e52f6b998c1976687edda2c85a80
logyQT <stoltmann96@gmail.com>
2022-10-24 22:25:08 +0200

    pobieranie

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const container = document.querySelector(".main");
 let e = document.querySelector("nav").querySelectorAll(".option");
 let currPage = undefined;
 
-
 e.forEach((e) =>
     e.addEventListener("click", (c) => {
         let page = c.target.getAttribute("page");
@@ -25,9 +24,9 @@ const changePage = (page) => {
     container.append(iframe);
 };
 
-changePage("home");
+const updateDownload = () => {
+    let link = document.querySelector(".link-list").value;
+    document.querySelector(".download-button").setAttribute("href", link);
+};
 
-const linki1 = ["https://github.com/TYPOWYSEB4/Graf_Edit/blob/main/%C2%A7eGraf%20Edit%20%C2%A7a1.6%20-%201.19.zip?raw=true", "link2", "link3"];
-document.querySelector(".lista1").addEventListener("change", (e) => {
-    document.querySelector(".pobierz1").setAttribute("href", linki1[e.target.value]);
-});
+changePage("home");
