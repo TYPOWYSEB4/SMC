ce588e36706663ba3a78c85a392e49a52f4008b6
logyQT <stoltmann96@gmail.com>
2022-12-02 16:30:18 +0100

    lang update

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const container = document.querySelector(".main");
 let e = document.querySelector("nav").querySelectorAll(".option");
 let currPage = undefined;
 
+const lang = window.localStorage.getItem("lang") ? window.localStorage.getItem("lang") : "pl";
+const language_selection = document.querySelector("select.language-selection");
+const language_selection_options = language_selection.querySelectorAll("option");
+
+language_selection.addEventListener("change", (e) => {
+    const selected_lang = language_selection.value;
+    window.localStorage.setItem("lang", selected_lang);
+    window.location.reload();
+});
+
+for (let i = 0; i < language_selection_options.length; i++) {
+    const val = language_selection_options[i].value;
+    if (val == lang) {
+        language_selection.selectedIndex = i;
+        break;
+    }
+}
+
 e.forEach((e) =>
     e.addEventListener("click", (c) => {
         let page = c.target.getAttribute("page");
@@ -18,7 +36,7 @@ const changePage = (page) => {
         temp.forEach((e) => e.remove());
     }
     let iframe = document.createElement("iframe");
-    iframe.setAttribute("src", `pages/${page}.html`);
+    iframe.setAttribute("src", `pages/${lang}/${page}.html`);
     iframe.setAttribute("onload", "this.insertAdjacentHTML('afterend', (this.contentDocument.body||this.contentDocument).innerHTML);this.remove()");
     iframe.setAttribute("style", "display: none");
     container.append(iframe);
